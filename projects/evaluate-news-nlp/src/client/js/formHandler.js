50d81bda2b5a6a6ba0dd3e69fdac78c5a20ef7af
Rucksack <6400803+Rucksack@users.noreply.github.com>
2020-05-21 21:03:01 +0200

    working Aylien API

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,3 +1,7 @@
+const baseURL = 'http://localhost:8080/';
+const sentiment = 'sentiment';
+const getResponse = 'get-response';
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -6,11 +10,30 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
+    
+    /*
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
     })
+    */
+    
+    fetch(baseURL+sentiment, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'text': formText
+        })
+    })
+    .then(res=>{
+        return res.json();
+    })
+    .then(res=>{
+        document.getElementById('results').innerHTML = JSON.stringify(res);
+    });
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,30 @@
-let path = require('path');
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('webpack-body-parser');
+
+const path = require('path');
 const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const aylien = require('aylien_textapi');
 
+const port = 8080;
+
+// Start up an instance of app
 const app = express()
 
+/* Middleware */
+// Where we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the dist folder
 app.use(express.static('dist'))
 
 // set aylien API credentials
@@ -15,26 +33,30 @@ let textapi = new aylien({
     application_key: process.env.API_KEY
 })
 
-textapi.sentiment({
-    'text': 'John is a very good football player!'
-}, function(error, response){
-    if (error === null){
-        console.log(response);
-    }
-});
-
 console.log(__dirname)
 
+// Routes
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8081!')
-})
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
+
+app.post('/sentiment', (req, res)=>{
+    textapi.sentiment(
+        req.body, 
+        (error, responseApi)=>{
+            if(error === null){
+                res.send(responseApi);
+            }
+        }
+    );
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(port, ()=>{
+    console.log(`server listening on port ${port}!`);
+});
